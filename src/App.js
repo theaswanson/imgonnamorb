6fc7691d63adba452321bd1a477b91eb76dad887
Adam Swanson <adam.swanson.98@gmail.com>
2023-02-21 20:51:50 -0500

    Add basic morbing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import morb from './morb.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>It's morbin' time.</p>
+      <p>I'm gonna morb!</p>
+      <p>Morbs all over.</p>
+      <img src={morb} alt="It's Morbin time." />
     </div>
   );
 }
